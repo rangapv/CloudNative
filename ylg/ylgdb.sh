80e54676e26e27f5fe45a0d63dae26af19a437ba
rangapv <rangapv@yahoo.com>
2023-04-17 14:47:33 +0000

    The Master DATABASE for generating the YAML file

--- ylg/ylgdb.sh
+++ ylg/ylgdb.sh
@@ -1,43 +1,42 @@
 #!/bin/bash
 #author: rangapv@yahoo.com 15-04-23
 
-declare -A spec 
-declare -A value 
+declare -A spec
+declare -A value
 
-spec[1]="apiVersion:"
-value[1]="Version"
-spec[2]="kind:"
-value[2]="kind"
-spec[3]=metadata:
-value[3]="Metadata"
-spec[3.4]=name:
-value[3.4]="Name"
-spec[3.5]=namespace:
-value[3.5]="Namespaces"
-spec[4.1]=replicas:
-value[4.1]="Replicas"
-spec[4]=spec:
-value[4]="spec"
-spec[4.21]=metadata:
-value[4.21]="metadata"
-spec[4.2]=template:
-value[4.2]="template"
-spec[4.211]=labels:
-value[4.211]="label"
-spec[4.2111]=app:
-value[4.2111]="apps"
-spec[4.31]=spec:
-value[4.31]="Spec"
-spec[4.311]=containers:
-value[4.311]="COntainers"
-spec[4.3111]="- name:"
-value[4.3111]="Name"
-spec[4.31112]=image:
-value[4.31112]="image"
-spec[4.31113]=args:
-value[4.31113]="args"
-spec[4.31114]=ports:
-value[4.31114]="ports"
-spec[4.311141]="- containerPorts:"
-value[4.311141]="containerports"
-
+spec[2]="apiVersion:"
+value[2]="Version"
+spec[3]="kind:"
+value[3]="kind"
+spec[4]=metadata:
+value[4]="Metadata"
+spec[4.4]=name:
+value[4.4]="Name"
+spec[4.5]=namespace:
+value[4.5]="Namespaces"
+spec[5.1]=replicas:
+value[5.1]="Replicas"
+spec[5]=spec:
+value[5]="spec"
+spec[5.21]=metadata:
+value[5.21]="metadata"
+spec[5.2]=template:
+value[5.2]="template"
+spec[5.211]=labels:
+value[5.211]="label"
+spec[5.2111]=app:
+value[5.2111]="apps"
+spec[5.31]=spec:
+value[5.31]="Spec"
+spec[5.311]=containers:
+value[5.311]="COntainers"
+spec[5.3111]="- name:"
+value[5.3111]="Name"
+spec[5.31112]=image:
+value[5.31112]="image"
+spec[5.31113]=args:
+value[5.31113]="args"
+spec[5.31114]=ports:
+value[5.31114]="ports"
+spec[5.311141]="- containerPorts:"
+value[5.311141]="containerports"
