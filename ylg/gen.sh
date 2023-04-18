703d2d5ab49b03150741e87b6f337c64786a734c
rangapv <rangapv@yahoo.com>
2023-04-18 06:38:41 +0000

    Added code for to user populate the YAML values

--- ylg/gen.sh
+++ ylg/gen.sh
@@ -5,6 +5,8 @@ set -E
 source <(curl -s https://raw.githubusercontent.com/rangapv/bash-source/main/s1.sh) >>/dev/null 2>&1
 #source "./ylgdb.sh"
 source <(curl -s https://raw.githubusercontent.com/rangapv/CloudNative/main/ylg/ylgdb.sh) >>/dev/null 2>&1
+chk=(spec metadata labels template args containers ports volumes)
+maskflag=0
 
 genylg() {
 int1=0
@@ -21,7 +23,8 @@ done | sort -n)
 
 #for a in "${sorted[@]}"; do echo "a is $a" ; done
 
-cln=`> r.yaml`
+cln="r.yaml"
+clnc=`> "$cln"`
 for a in "${sorted[@]}"
 do 
 #echo "a si $a num3 is $num3" 
@@ -31,25 +34,55 @@ num2=${#num1}
 num3=$((num2-=1))
        if [[ (($num3 -eq 0)) ]]
        then
-               echo "${spec[$a]}"  >>r.yaml
+               echo "${spec[$a]}"  >>"$cln"
        elif [[ (($num3 -eq 1)) ]]
        then
 
-               echo "  ${spec[$a]}" >>r.yaml
+               echo "  ${spec[$a]}" >>"$cln"
        elif [[ (($num3 -gt 1 )) ]]
        then
 	for ((i=1; i <= $num3; i++));
         do 
-	 echo -n "  " >>r.yaml
+	 echo -n "  " >>"$cln"
         done
-        echo "${spec[$a]}" >>r.yaml
+        echo "${spec[$a]}" >>"$cln"
        else
 	       echo "  "
        fi
 done
 
+}
 
+chkspec() {
+
+chspeck=("$@")
+for b in "${chk[@]}"
+do
+ if [[ "$b" == "$chspeck" ]]
+ then
+       	maskflag=1
+ fi
+done
+}
+
+filyl() {
+
+fln="v.yaml"
+flnc=`> "$fln"`
+
+for a in "${sorted[@]}"
+do
+	chkspec "${spec[$a]:0:-1}"
+	if [[ (( $maskflag -eq 0 )) ]]
+	then
+	echo "${value[$a]}:" >>"$fln"
+        fi
+	maskflag=0
+done
 }
 
 genylg
-
+if [[ ! -z "$cln" ]]
+then
+	filyl
+fi
