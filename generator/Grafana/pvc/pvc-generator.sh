c1853523421825cab1d02b1b126de145e0a826d3
rangapv <rangapv@yahoo.com>
2023-07-31 06:25:36 +0000

    Updated Grafan script to reflect code optimization of workhorses(gen & fill)

--- generator/Grafana/pvc/pvc-generator.sh
+++ generator/Grafana/pvc/pvc-generator.sh
@@ -2,19 +2,14 @@
 
 set -E
 
-#array values that needs to be skipped in the top section befor spec no entries means no values to skipp all present
-#it is being called at checkspec in the pvgen function
-skippm=(type annotations)
-#array values that needs to be skipped fromt eh database in the spec section
-#it is being called at function findp, pvfilyl(pvspec11) , pvfill(pvgen)
-skippv=(capacity nfs csi)
-
 #The YAMl file name for this resource ; this can be changed
 pvfile="pvc.yaml"
 #The Values file name for this resource ; this can be changed
 pvvfile="pvcv.yaml"
 #The starting Index in the database(ylgdb.sh) for this resource ; this CANNOT be changed
 myindx="5"
+myindx="1,2,3(type annotations),5(capacity nfs csi)"
+
 
 if [[ "$1" == "gen" ]]
 then
--- generator/Grafana/service/service-generator.sh
+++ generator/Grafana/service/service-generator.sh
@@ -2,19 +2,13 @@
 
 set -E
 
-#array values that needs to be skipped in the top section befor spec no entries means no values to skipp all present
-#it is being called at checkspec in the pvgen function
-skippm=(labels )
-#array values that needs to be skipped fromt eh database in the spec section
-#it is being called at function findp, pvfilyl(pvspec11) , pvfill(pvgen)
-skippv=( )
-
 #The YAMl file name for this resource ; this can be changed
 pvfile="svr.yaml"
 #The Values file name for this resource ; this can be changed
 pvvfile="svv.yaml"
 #The starting Index in the database(ylgdb.sh) for this resource ; this CANNOT be changed
-myindx="6"
+myindx="1,2,3(labels),6"
+
 
 if [[ "$1" == "gen" ]]
 then
