793722742a830fd2c505a2fa4eb7616d4e319c3e
rangapv <rangapv@yahoo.com>
2023-07-31 06:42:24 +0000

    Updated myindx

--- generator/Prometheus/pvc/pvc-generator.sh
+++ generator/Prometheus/pvc/pvc-generator.sh
@@ -7,7 +7,7 @@ pvfile="pvc.yaml"
 #The Values file name for this resource ; this can be changed
 pvvfile="pvcv.yaml"
 #The starting Index in the database(ylgdb.sh) for this resource ; 
-myindx="1,2,3,5(capacity nfs csi)"
+myindx="1,2,3(type annotations),5(capacity nfs csi)"
 
 if [[ "$1" == "gen" ]]
 then
