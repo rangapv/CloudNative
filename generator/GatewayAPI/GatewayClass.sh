c616aa78373ddd3556f8590d3922685edc420754
rangapv <rangapv@yahoo.com>
2023-08-11 11:16:13 +0000

    Generated PERFECT GatewayClass file from the YLGDB database

--- generator/GatewayAPI/GatewayClass.sh
+++ generator/GatewayAPI/GatewayClass.sh
@@ -7,7 +7,7 @@ pvfile="dgr.yaml"
 #The Values file name for this resource ; this can be changed
 pvvfile="dgv.yaml"
 #The starting Index in the database(ylgdb.sh) for this resource ; this can have multiple indexes depending on your particular use case
-myindx="1,2,3(type annotations),37"
+myindx="1,2,3(labels type annotations),37"
 
 if [[ "$1" == "gen" ]]
 then
