ddc9a4d3e13cb9602e7b525c3b168aed13dafca7
rangapv <rangapv@yahoo.com>
2023-08-20 12:13:16 +0000

    istio Gateway httpRoute generators and yamls

--- generator/GatewayAPI/istio/httpRoute/httpRoute.sh
+++ generator/GatewayAPI/istio/httpRoute/httpRoute.sh
@@ -7,17 +7,17 @@ pvfile="dgr.yaml"
 #The Values file name for this resource ; this can be changed
 pvvfile="dgv.yaml"
 #The starting Index in the database(ylgdb.sh) for this resource ; this can have multiple indexes depending on your particular use case
-myindx="1,2,3(type annotations),39"
+myindx="1,2,3(type annotations labels app),39(headers queryParams method set remove responseHeaderModifier requestMirror requestRedirect urlRewrite extensionRef)"
 
 if [[ "$1" == "gen" ]]
 then
-        #source /home/ubuntu/cn/generator/gen.sh "gen" "$pvfile" "$pvvfile" "$myindx"
-	source <(curl -s https://raw.githubusercontent.com/rangapv/CloudNative/main/generator/gen.sh) "gen" "$pvfile" "$pvvfile" "$myindx"
+        source /home/ubuntu/cn/generator/gen.sh "gen" "$pvfile" "$pvvfile" "$myindx"
+	#source <(curl -s https://raw.githubusercontent.com/rangapv/CloudNative/main/generator/gen.sh) "gen" "$pvfile" "$pvvfile" "$myindx"
 
 elif [[ "$1" == "fill" ]]
 then
-	#source /home/ubuntu/cn/generator/gen.sh "fill" "$pvfile" "$pvvfile" "$myindx"
-	source <(curl -s https://raw.githubusercontent.com/rangapv/CloudNative/main/generator/gen.sh) "fill" "$pvfile" "$pvvfile" "$myindx"
+	source /home/ubuntu/cn/generator/gen.sh "fill" "$pvfile" "$pvvfile" "$myindx"
+	#source <(curl -s https://raw.githubusercontent.com/rangapv/CloudNative/main/generator/gen.sh) "fill" "$pvfile" "$pvvfile" "$myindx"
 else
 	echo "usuage: httpRoute.sh gen/fill"
 
