a359094fe74bd318314b0e8552649ca90f7ca933
rangapv <rangapv@yahoo.com>
2023-10-23 09:56:25 +0000

    42 spec modification and generator for the ClusteIssuer ACME verification unit test

--- generator/CertManager/ClusterIssuer/ClusterIssuer.sh
+++ generator/CertManager/ClusterIssuer/ClusterIssuer.sh
@@ -7,7 +7,7 @@ pvfile="cih.yaml"
 #The Values file name for this resource ; this can be changed
 pvvfile="cihv.yaml"
 #The starting Index in the database(ylgdb.sh) for this resource ; this can have multiple indexes depending on your particular use case
-myindx="1,2,3(labels type annotations),42"
+myindx="1,2,3(labels type annotations),42(podTemplate ingressTemplate)"
 
 
 if [[ "$1" == "gen" ]]
@@ -22,6 +22,6 @@ then
 	#source /home/ubuntu/cn/generator/gen.sh "fill" "$pvfile" "$pvvfile" "$myindx"
 	source <(curl -s https://raw.githubusercontent.com/rangapv/CloudNative/main/generator/gen.sh) "fill" "$pvfile" "$pvvfile" "$myindx"
 else
-	echo "usuage: http.sh gen/fill"
+	echo "usage: ClusterIssuer.sh gen/fill"
 
 fi
