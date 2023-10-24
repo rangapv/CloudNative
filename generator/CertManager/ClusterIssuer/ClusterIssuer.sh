981ab2bcb9b792afb3e4907cda0efb79bcf514da
rangapv <rangapv@yahoo.com>
2023-10-24 11:05:21 +0000

    Created the ClusterIssuer for ACME cert manager and unit tested

--- generator/CertManager/ClusterIssuer/ClusterIssuer.sh
+++ generator/CertManager/ClusterIssuer/ClusterIssuer.sh
@@ -7,7 +7,7 @@ pvfile="cih.yaml"
 #The Values file name for this resource ; this can be changed
 pvvfile="cihv.yaml"
 #The starting Index in the database(ylgdb.sh) for this resource ; this can have multiple indexes depending on your particular use case
-myindx="1,2,3(labels type annotations),42(podTemplate ingressTemplate)"
+myindx="1,2,3(labels type annotations),42(podTemplate ingressTemplate externalAccountBinding gatewayHTTPRoute)"
 
 
 if [[ "$1" == "gen" ]]
